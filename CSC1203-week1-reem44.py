baea0133e59a29f24f22895e528cea3139314cb0
reemalanzi44 <reemalanzi011@gmail.com>
2026-01-31 04:15:18 +0300

    Add files via upload

---
+++ CSC1203-week1-reem44.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+print(" Welcome to python programming") #print welcome message
+print ('reem alanzi') #print personal info on separate lines
+print('course name: Advanced programming') 
+
+
+# In[22]:
+
+
+student_name ="reem"
+age = 21
+gpa =4.0
+print(student_name , type(student_name))
+print(age, type(age))
+print(gpa, type(gpa))
+
+
+# In[25]:
+
+
+subjects=["Math","Python","Physics"] 
+student_info = {"id":44100 , "year":3}
+skills = {"Python", "java"}
+is_enrolled = True
+graduation_date = None
+print( subjects   , type(subjects))
+print( student_info   , type(student_info))
+print( skills    , type(skills))
+print(is_enrolled   , type(is_enrolled))
+print(graduation_date    , type(graduation_date))
+
