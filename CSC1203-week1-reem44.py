f0fe46475f9437a0b31faec5a3ef9993803998bb
reemalanzi44 <reemalanzi011@gmail.com>
2026-02-01 06:39:04 +0300

    Update CSC1203-week1-reem44.py

--- CSC1203-week1-reem44.py
+++ CSC1203-week1-reem44.py
@@ -15,19 +15,15 @@ print('course name: Advanced programming')
 student_name ="reem"
 age = 21
 gpa =4.0
-print(student_name , type(student_name))
-print(age, type(age))
-print(gpa, type(gpa))
-
-
-# In[25]:
-
-
 subjects=["Math","Python","Physics"] 
 student_info = {"id":44100 , "year":3}
 skills = {"Python", "java"}
 is_enrolled = True
 graduation_date = None
+
+print(student_name , type(student_name))
+print(age, type(age))
+print(gpa, type(gpa))
 print( subjects   , type(subjects))
 print( student_info   , type(student_info))
 print( skills    , type(skills))
